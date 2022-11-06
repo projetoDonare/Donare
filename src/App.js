e8c9e2ce169ba6fb861771027d22e29359169251
Casimiro Thompson <casimirothompson@gmail.com>
2022-11-06 19:12:26 -0300

    espero que tenha colocado o node module

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          fiz uma grande modificação aqui, vamos ver se funciona.
         </p>
         <a
           className="App-link"
