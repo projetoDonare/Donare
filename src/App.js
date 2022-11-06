00e734aca991f51db80739594f2d4657577bfde6
Casimiro Thompson <casimirothompson@gmail.com>
2022-11-06 19:54:33 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          fiz uma grande modificação aqui, vamos ver se funciona.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <p>
+        ola tudo bem?
+      </p>
+      </div>
   );
 }
 
