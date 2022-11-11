aa21434363ec875e996b522a37bfee3a154eedb3
Casimiro Thompson <casimirothompson@gmail.com>
2022-11-11 18:52:55 -0300

    adicionando imagem no componente 4 da home

--- src/Components/Home/HomePt4.jsx
+++ src/Components/Home/HomePt4.jsx
@@ -1,8 +1,17 @@
 import styles from '../../Css/Home-css/HomePt4.module.css'
-function HomePt4(){
-    return(
+import sejaVoluntario from '../../Images/imagem_sejaVoluntario.png'
+import SejaDoador from '../../Images/image_sejaDoador.png'
+
+
+function HomePt4() {
+    return (
         <section className={styles.Home}>
-home4
+            <div className={styles.div_imagem}>
+                <img src={sejaVoluntario} alt="" />
+            </div>
+            <div className={styles.div_imagem}>
+                <img src={SejaDoador} alt="" />
+            </div>
         </section>
     )
 }
