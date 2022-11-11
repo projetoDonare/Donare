e1624838280d2f9c994c5cae3a34665f9fc2f1f0
Ícaro Anjos <anjosicarosilva@gmail.com>
2022-11-11 16:50:00 -0300

    Arrumando os componentes da Home

--- src/Components/Home/HomePt2.jsx
+++ src/Components/Home/HomePt2.jsx
@@ -6,13 +6,12 @@ function HomePt2() {
       <div className={styles.borda}></div>
 
 <div className={styles.txt}>
-  <h1>titulo</h1>
+  <h1>Cerca de 27 milões de toneladas de alimentos vão para o lixo todos os dias no Estado de São Paulo</h1>
+  <br />
   <div className={styles.linha}></div>
+  <br />
   <p>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut sunt non,
-    nobis iure autem minus ratione architecto. Quos, reprehenderit ratione
-    placeat atque, perspiciatis accusantium voluptatum nobis modi nostrum
-    deserunt iusto.
+  Os mercados e restaurantes são responsaveis por x quantidades de alimentos em boas condiçoes que são descartados, boa parte desses alimentos são jogados no lixo por x motivos. Nós buscamos diminuir a desigualdade alimentar através da doação dos mercados que por algum motivo não irão comercializar um alimento em bom estado.
   </p>
 </div>
     </section>
