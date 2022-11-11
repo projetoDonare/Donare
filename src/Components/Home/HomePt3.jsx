f734f911e384c1ab280eed8c975f54543c89a40e
Casimiro Thompson <casimirothompson@gmail.com>
2022-11-11 16:26:09 -0300

    .

--- src/Components/Home/HomePt3.jsx
+++ src/Components/Home/HomePt3.jsx
@@ -1,20 +1,9 @@
 import styles from '../../Css/Home-css/HomePt3.module.css'
-function HomePt3(){
-    return(
-        <section className={styles.Home}>
-     <div className={styles.borda}></div>
-
-<div className={styles.txt}>
-  <h1>titulo</h1>
-  <div className={styles.linha}></div>
-  <p>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut sunt non,
-    nobis iure autem minus ratione architecto. Quos, reprehenderit ratione
-    placeat atque, perspiciatis accusantium voluptatum nobis modi nostrum
-    deserunt iusto.
-  </p>
-</div>
-        </section>
-    )
+function HomePt3() {
+  return (
+    <section className={styles.Home}>
+      
+    </section>
+  )
 }
 export default HomePt3
