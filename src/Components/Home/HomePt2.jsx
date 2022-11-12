b5ed83e9dd5a570fbf3fc71192af174b786d5611
Pedro Lazaro <donaredemoday@gmail.com>
2022-11-12 16:33:44 -0300

    Mudança na logo

--- src/Components/Home/HomePt2.jsx
+++ src/Components/Home/HomePt2.jsx
@@ -5,15 +5,15 @@ function HomePt2() {
     <section className={styles.Home}>
       <div className={styles.borda}></div>
 
-<div className={styles.txt}>
-  <h1>Cerca de 27 milhões de toneladas de alimentos vão para o lixo todos os dias no Estado de São Paulo</h1>
-  <br />
-  <div className={styles.linha}></div>
-  <br />
-  <p>
-  Os mercados e restaurantes são responsaveis por x quantidades de alimentos em boas condiçoes que são descartados, boa parte desses alimentos são jogados no lixo por x motivos. Nós buscamos diminuir a desigualdade alimentar através da doação dos mercados que por algum motivo não irão comercializar um alimento em bom estado.
-  </p>
-</div>
+      <div className={styles.txt}>
+        <h1>Cerca de 27 milhões de toneladas de alimentos vão para o lixo todos os dias no Estado de São Paulo</h1>
+        <br />
+        <div className={styles.linha}></div>
+        <br />
+        <p>
+          Os mercados e restaurantes são responsaveis por x quantidades de alimentos em boas condiçoes que são descartados, boa parte desses alimentos são jogados no lixo por x motivos. Nós buscamos diminuir a desigualdade alimentar através da doação dos mercados que por algum motivo não irão comercializar um alimento em bom estado.
+        </p>
+      </div>
     </section>
   );
 }
