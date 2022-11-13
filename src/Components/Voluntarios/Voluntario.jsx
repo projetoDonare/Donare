82b00169a72a32a7dca177321fead9cf186a0bae
Pedro Lazaro <donaredemoday@gmail.com>
2022-11-13 20:52:24 -0300

    feita começo da pagina voluntarios

--- src/Components/Voluntarios/Voluntario.jsx
+++ src/Components/Voluntarios/Voluntario.jsx
@@ -1,9 +1,16 @@
 import styles from '../../Css/Voluntario_css/Voluntario.module.css'
+import img_voluntario1 from '../../Images/img_voluntario1.png'
 function Voluntario() {
     return (
         <>
             <section className={styles.page1}>
-                <h1>sessao voluntario</h1>
+                <div className={styles.txt}>
+                    <h1>Existem muitas formas de ajudar o próximo</h1>
+                    <h2>Veja como você pode se voluntariar</h2>
+                </div>
+                <div className={img_voluntario1}>
+                    
+                </div>
             </section>
         </>
     );
