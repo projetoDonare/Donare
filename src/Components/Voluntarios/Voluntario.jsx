190acf509d2a69e4f22a956d99db05ee7e3c983c
Pedro Lazaro <donaredemoday@gmail.com>
2022-11-13 19:57:35 -0300

    .

---
+++ src/Components/Voluntarios/Voluntario.jsx
@@ -0,0 +1,18 @@
+import styles from '../../Css/Voluntario_css/Voluntario.module.css'
+import VoluntarioPt2 from './VoluntarioPt2'
+import VoluntarioPt3 from './VoluntarioPt3'
+
+function Voluntario() {
+    return(
+        <>
+        <section className={styles.Home}>
+            <div className={styles.txt}>
+                <h1>Você sabia que 58,7% do Brasil possui insegurança alimentar?</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur reprehenderit commodi eveniet quod tempore eaque sequi dolore ea, molestiae explicabo deserunt amet ducimus. Fugiat itaque eius ullam. Maxime, dicta commodi.</p>
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Voluntario
