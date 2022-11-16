db47b9ed64e4a5cbbbbe22aaac8bd933c6e9293e
Casimiro Thompson <casimirothompson@gmail.com>
2022-11-16 05:41:58 -0300

    parte 1 e 3 da page instituiçoes feita, porem falta a parte 2 (parte do carrocel)

--- src/Components/Instituicoes/Instituicoes.jsx
+++ src/Components/Instituicoes/Instituicoes.jsx
@@ -1,18 +1,15 @@
 import styles from '../../Css/Instituicoes/Instituicoes.module.css'
-import InstituicoesPt2 from './Instituicoespt2'
+import InstituicoesPt3 from './Instituicoespt3'
 
 function Instituicoes() {
     return (
         <>
-            <section className={styles.nstituicoes}>
-                <div className={styles.txt}>
-                    <h1>Você pode encher esse prato</h1>
-                    <p>
-                        Conheça as Ongs e Igrejas que lutam contra a fome que mata milhares de brasileiros
-                    </p>
-                </div>
+            <section className={styles.Header_invisivel}></section>
+            <section className={styles.InstituicoesPt1}>
+                <h1>Você pode encher esse prato</h1>
+                <p>Conheça as Ongs e Igrejas que lutam contra a fome que mata milhares de brasileiros</p>
             </section>
-            <InstituicoesPt2 />
+            <InstituicoesPt3 />
         </>
     )
 }
---
+++ src/Components/Instituicoes/Instituicoespt3.jsx
@@ -0,0 +1,16 @@
+import styles from '../../Css/Instituicoes/InstituicoesPt3.module.css'
+
+function InstituicoesPt3() {
+    return (
+        <section className={styles.InstituicoesPt3}>
+            <h1>
+                Você pode encher esse prato
+            </h1>
+            <p>
+                Conheça as Ongs e Igrejas que lutam contra a fome que mata milhares de brasileiros
+            </p>
+        </section>
+    );
+};
+
+export default InstituicoesPt3;
