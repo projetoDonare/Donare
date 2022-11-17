46461e592d51b2616aacce97bf9687e74cc8a548
Ícaro Anjos <anjosicarosilva@gmail.com>
2022-11-16 23:00:02 -0300

    .

--- src/Components/Projeto/Projeto.jsx
+++ src/Components/Projeto/Projeto.jsx
@@ -8,10 +8,10 @@ function Projeto(){
          <div className={styles.txt}>
          <h1>O Projeto Donare</h1>
             <br />
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ratione sequi cum voluptatum, unde facilis animi illum dolore soluta labore sapiente ipsa et, recusandae quam voluptas aut veritatis voluptates provident?</p>
+           
          </div>
         </section>
         </>
     )
 }
-export default Projeto
+export default Projeto;
