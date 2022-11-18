a144d101dd16133ac8b82c9e2509f96d086b2c0e
Pedro Lazaro <donaredemoday@gmail.com>
2022-11-17 21:02:02 -0300

    começado perfil ong

--- src/Components/PerfilOng/PerfilOng.jsx
+++ src/Components/PerfilOng/PerfilOng.jsx
@@ -1,4 +1,5 @@
 import styles from '../../Css/PerfilOng_css/PerfilOng.module.css'
+import imagem_ong from '../../Images/imagem_ong.png'
 import PerfilOngPt2 from './PerfilOngPt2'
 import PerfilOngPt3 from './PerfilOngPt3'
 
@@ -6,7 +7,20 @@ function PerfilOng() {
     return (
         <>
             <section className={styles.page1}>
+                <div className={styles.txt1}>
+                    <img src={imagem_ong} alt="" />
+                    <h1>Associação do Menino Jesus</h1>
+                    <p>Telefone:  (11) 3535-3535</p>
+                    <p className={styles.email}>E-mail: meninojesus@gmail.com</p>
 
+                    <div className={styles.txt2}>
+                        <p>Texto sobre a ong, o que ela faz, quais suas metas e etc</p>
+                    </div>
+
+                    <div className={styles.borda}></div>
+
+                </div>
+    
             </section>
             <PerfilOngPt2 />
             <PerfilOngPt3 />
